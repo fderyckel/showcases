8a49029f5d2d0cd821775134a51a05d69fa481c9
fderyckel <f.deryckel@gmail.com>
2015-01-06 08:12:04 +0200

    minor change for Joburg files Mainly add comments + update prices of fruits.

--- JoburgMarket/PricesJoburgApples.R
+++ JoburgMarket/PricesJoburgApples.R
@@ -1,6 +1,8 @@
 library(XML)
 library(dplyr)
 prices <- data.frame(readHTMLTable("http://www.joburgmarket.co.za/dailyprices.php?commodity=90&containerall=2"))
+
+#add the date
 prices <- mutate(prices,Sys.Date())
 colnames(prices) <- c("Container", "UnitMass", "ProductName", "TotalValueSold", "TotalQuantitySold", "TotalKgSold", "AveragePrice", "HighestPrice", "AveragePriceperKg", "HighestPriceperKg", "Date")
 
@@ -8,11 +10,13 @@ colnames(prices) <- c("Container", "UnitMass", "ProductName", "TotalValueSold", 
 prices[,4] <- gsub(",", "", prices[,4])
 
 #take away the R use for the Rand symbol
+#take away some unnecessary columns that I can calculate myself
+#only consider the 18.5 Kg boxes and take out the Cl2 products that are 18.5 Kg boxe
 prices[,c(2,4,7:10)] <- as.numeric(gsub("R", "", as.matrix(prices[,c(2,4,7:10)])))
 prices <- select(prices, -AveragePriceperKg, -HighestPriceperKg, -UnitMass)
 prices <- prices %>% filter(TotalValueSold>0) %>% filter(!grepl("CL 2", ProductName), Container == "18.50KG CARTON" ) %>% arrange(TotalQuantitySold)
 
 prices
 
-
+#write the df into a file, without writing row names and colum names.
 write.table(prices, file="JoburgApples.csv", sep=",", col.names=F, row.names=F, append=TRUE)
