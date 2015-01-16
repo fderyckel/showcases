d06ccdd913a0e87d16e3c42a43e456ad90093c5d
fderyckel <f.deryckel@gmail.com>
2015-01-16 21:03:48 +0200

    Change how to handle blank value, add the testname

--- MAP EDA & PA/script01 - read data.R
+++ MAP EDA & PA/script01 - read data.R
@@ -1,30 +1,41 @@
+library(dplyr)
+
 ##read the .txt file.  
 ## it is a tab separated values, and we have to make explicit the encoding
-map <- read.table(source, sep = "\t", header=T, fileEncoding="UTF-16")
+## also make explicit the missing values are ""
+## make sure the sourcefile is set first
+## sourcefile = "~/Documents/R/MAP Predictive Analytics/RawData/2009 09 Fall.txt"
+map <- read.table(sourcefile, sep = "\t", header=T, fileEncoding="UTF-16", na.strings="")
 
-#check its structure & view the first few rows
-str(data)
-head(data)
+#we'll do some cleaning
+map[,13] = as.Date(map[,13], format = "%m/%d/%Y")
+map[,c(15, 37, 38)] <- as.integer(as.matrix(map[,c(15, 37, 38)]))
+
+#let's already write on disk this cleaner, raw-ish version into a .csv format
+# + just take out names (for privacy)
+filename = paste0(map[1,6], "raw.csv")
+map <- select(map, -StudentName)
+write.table(map, file= filename, sep=",", col.names=T, row.names=F)
 
 #take out all the columns of no interest + take out names (for privacy)
-library(dplyr)
-data <- map %>% select(-RITtoReadingMin, -StudentName, -RITtoReadingMax, -TestName, -StudentEthnic, -TestTypeName, -InstitutionName, -TestStartDate, -RITtoReadingScore)
+data <- map %>% select(-RITtoReadingMin, -RITtoReadingMax, -StudentEthnic, -TestTypeName, -InstitutionName, -TestStartDate, -RITtoReadingScore)
+
 
 ##reconstruct a wide df first
 #separate by Measurement Scale & rename the column
 math<- data %>% filter(MeasurementScaleName == "Mathematics") %>% 
-  rename(mathRIT = TestRITScore, mathPercentile = TestPercentile, mathStdErr = TestStdErr, Computation = GoalRITScore1, NumberSense = GoalRITScore2, Geometry = GoalRITScore3, Measurement = GoalRITScore4, StatProb = GoalRITScore5, Algebra = GoalRITScore6, ProblemSolving = GoalRITScore7) %>% 
+  rename(mathTestName = TestName, mathRIT = TestRITScore, mathPercentile = TestPercentile, mathStdErr = TestStdErr, Computation = GoalRITScore1, NumberSense = GoalRITScore2, Geometry = GoalRITScore3, Measurement = GoalRITScore4, StatProb = GoalRITScore5, Algebra = GoalRITScore6, ProblemSolving = GoalRITScore7) %>% 
   select(-MeasurementScaleName, -GoalName1, -GoalName2, -GoalName3, -GoalName4, -GoalName5, -GoalName6, -GoalName7) %>% 
   rename(ComputationAdj = GoalAdjective1, NumberSenseAdj = GoalAdjective2, GeometryAdj = GoalAdjective3, MeasurementAdj = GoalAdjective4, StatProbAdj = GoalAdjective5, AlgebraAdj = GoalAdjective6, ProblemSolvingAdj = GoalAdjective7)
 
 #we can take out more columns here as we'll bind these data later on.
 lang <- data %>% filter(MeasurementScaleName == "Language Usage") %>% 
-  rename(langRIT = TestRITScore, langPercentile = TestPercentile, langStdErr = TestStdErr, GramUs = GoalRITScore1, SpPunctCap = GoalRITScore2, ParaSent = GoalRITScore3, GenPurpAud = GoalRITScore4) %>% 
+  rename(langTestName = TestName, langRIT = TestRITScore, langPercentile = TestPercentile, langStdErr = TestStdErr, GramUs = GoalRITScore1, SpPunctCap = GoalRITScore2, ParaSent = GoalRITScore3, GenPurpAud = GoalRITScore4) %>% 
   select(-MeasurementScaleName, -GoalName1, -GoalName2, -GoalName3, -GoalName4, -GoalName5, -GoalName6, -GoalName7, -GoalAdjective5, -GoalAdjective6, -GoalAdjective7, -GoalRITScore5, -GoalRITScore6, -GoalRITScore7) %>% 
   rename(GramUsAdj = GoalAdjective1, SpPunctCapAdj = GoalAdjective2, ParaSentAdj = GoalAdjective3, GenPurpAudAdj = GoalAdjective4)
 
 read <- data %>% filter(MeasurementScaleName == "Reading") %>% 
-  rename(readRIT = TestRITScore, readPercentile = TestPercentile, readStdErr = TestStdErr, ReadProc = GoalRITScore1, LitTex = GoalRITScore2, NFictTex = GoalRITScore3) %>% 
+  rename(readTestName = TestName, readRIT = TestRITScore, readPercentile = TestPercentile, readStdErr = TestStdErr, ReadProc = GoalRITScore1, LitTex = GoalRITScore2, NFictTex = GoalRITScore3) %>% 
   select(-MeasurementScaleName, -GoalName1, -GoalName2, -GoalName3, -GoalName4, -GoalName5, -GoalName6, -GoalName7, -GoalAdjective4, -GoalAdjective5, -GoalAdjective6, -GoalAdjective7, -GoalRITScore4, -GoalRITScore5, -GoalRITScore6, -GoalRITScore7) %>% 
   rename(ReadProcAdj = GoalAdjective1, LitTexAdj = GoalAdjective2, NFictTexAdj = GoalAdjective3)
 
@@ -40,4 +51,5 @@ sum(is.na(map$langRIT))
 sum(is.na(map$readRIT))
 
 #save everything into a new .csv file
-write.table(map, file="map.csv", sep=",", col.names=T, row.names=F)
+filename = paste0(map[1,4], "wide.csv")
+write.table(map, file = filename, sep=",", col.names = T, row.names = F)
